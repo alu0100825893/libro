05f965c18cddbe134c879f601fb5c404068c6376
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-03-28 23:37:09 +0100

    sass removed

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
-/*
- * Module dependencies
- */
 var express = require('express');
 var app = express();
-
 
 var path = require('path');
 var favicon = require('serve-favicon');
 
+/*
 var sassMiddleware = require('node-sass-middleware');    
-
+*/
 
 app.set('port', (process.env.PORT || 5000));
 
-
-
-app.use("/stylesheets", express.static(__dirname + '/views'));
 app.use(
   express.static(__dirname + '/public')
 );
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-app.use(
-  express.static(__dirname + '/views')
-);
-
-
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
@@ -34,6 +22,7 @@ app.set('view engine', 'pug')
 
 
 //SASS
+/*
 app.use(
     sassMiddleware({
         src: __dirname + '/public/stylesheets', 
@@ -42,17 +31,8 @@ app.use(
         debug: true // obvious
     })
 );
-
-//Coffee
-/*
-  app.use(
-  coffeeMiddleware({
-        src: __dirname, // where coffescripts are
-        dest: __dirname + '/views', // where js sould go
-        debug: true
-  })
-);
 */
+
 app.get('/', function (req, res) {
   res.render('index', { title : 'Home' }
   )
@@ -61,7 +41,6 @@ app.get('/', function (req, res) {
 app.get('/test', function (request, response) {
    response.render('test', { title: 'test' });
 });
-
 
 app.listen(app.get('port'), function () {
     console.log("Node app is running at localhost:" + app.get('port'));
